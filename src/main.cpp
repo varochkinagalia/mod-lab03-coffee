e83e347f18a145bc4ae454e5a3d5fe9535d2169b
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-03-30 21:32:44 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,43 @@
-
+// Copyright 2022 UNN-IASR
+#include "Automata.h"
+#include <iostream>
+using namespace std;
+int main() {
+	Automata automat;
+	automat.on();
+  
+	automat.getMenu();
+	automat.choice(2);
+	automat.coin(10);
+	automat.check();
+	automat.cook();
+	automat.finish();
+  
+	cout << endl;
+  
+	automat.getMenu();
+	automat.choice(2);
+	automat.coin(5);
+	automat.check();
+	automat.coin(10);
+	automat.check();
+	automat.cook();
+	automat.finish();
+  
+	cout << endl;
+  
+	automat.getMenu();
+	automat.choice(3);
+	automat.coin(50);
+	automat.cancel();
+  
+	cout << endl;
+  
+	automat.coin(4);
+	automat.choice(4);
+	automat.getState();
+  
+	automat.off();
+  
+	return 0;
+}
