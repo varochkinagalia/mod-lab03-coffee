f8cc7977782466071048c7938114766e2a485753
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-03-30 22:11:55 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
 	automat.check();
 	automat.cook();
 	automat.finish();
-	std::cout << endl;
+	std::cout << std::endl;
 	automat.getMenu();
 	automat.choice(2);
 	automat.coin(5);
@@ -19,12 +19,12 @@ int main() {
 	automat.check();
 	automat.cook();
 	automat.finish();
-	std::cout << endl;
+	std::cout << std::endl;
 	automat.getMenu();
 	automat.choice(3);
 	automat.coin(50);
 	automat.cancel();
-	std::cout << endl;
+	std::cout << std::endl;
 	automat.coin(4);
 	automat.choice(4);
 	automat.getState();
