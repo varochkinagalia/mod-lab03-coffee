a858be8dfcc89b04336ae43591e0aa71a3b26571
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-03-30 19:31:53 +0300

    tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,87 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(AutomataTest, InitialState) {
+    Automata automata;
+    EXPECT_EQ(automata.getState(), "off");
+}
+
+TEST(AutomataTest, InsertCoin) {
+    Automata automata;
+    automata.on();
+    automata.coin(3);
+    EXPECT_EQ(automata.getState(), "accept");
+}
+
+TEST(AutomataTest, InsertCoin) {
+    Automata automata;
+    automata.on();
+    automata.coin(3);
+    automata.coin(2);
+    EXPECT_EQ(automata.getState(), "accept");
+}
+
+TEST(AutomataTest, Select) {
+    Automata automata;
+    automata.on();
+    automata.coin(10);
+    automata.choice(2);
+    EXPECT_EQ(automata.getState(), "check");
+}
+
+TEST(AutomataTest, SelectWithCancel) {
+    Automata automata;
+    automata.on();
+    automata.coin(15);
+    automata.cancel();
+    EXPECT_EQ(automata.getState(), "wait");
+}
+
+TEST(AutomataTest, SelectWithMoneyWithCancel) {
+    Automata automata;
+    automata.on();
+    automata.coin(10);
+    automata.choice(2);
+    automata.cancel();
+    EXPECT_EQ(automata.getState(), "wait");
+}
+
+TEST(AutomataTest, Check) {
+    Automata automata;
+    automata.on();
+    automata.coin(10);
+    automata.choice(2);
+    automata.check();
+    EXPECT_EQ(automata.getState(), "check");
+}
+
+TEST(AutomataTest, Cooking) {
+    Automata automata;
+    automata.on();
+    automata.coin(5);
+    automata.choice(2);
+    automata.cook(2);
+    EXPECT_EQ(automata.getState(), "cook");
+}
+
+TEST(AutomataTest, CookingWithFinish) {
+    Automata automata;
+    automata.on();
+    automata.coin(5);
+    automata.choice(2);
+    automata.cook(2);
+    automata.finish();
+    EXPECT_EQ(automata.getState(), "wait");
+}
+
+TEST(AutomataTest, Off) {
+    Automata automata;
+    automata.on();
+    automata.coin(5);
+    automata.choice(2);
+    automata.cook(2);
+    automata.finish();
+    automata.off();
+    EXPECT_EQ(automata.getState(), "off");
+}
