e4f4bfe68dd14d6ecab491b2b2406cee7828cb16
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-03-30 20:27:02 +0300

    Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,26 +1,27 @@
-#ifndef AUTOMATA_H
-#define AUTOMATA_H
+#ifndef INCLUDE_AUTOMATA_H_
+#define INCLUDE_AUTOMATA_H_
 #include <string>
 enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
 class Automata
 {
 public:
-	Automata();
-	void on();
-	void off();
-	void coin(int amount);
-	void getMenu();
-	void getState();
-	void choice(int drink);
-	void check();
-	void cancel();
-	void cook();
-	void finish();
+Automata();
+void on();
+void off();
+void coin(int amount);
+void getMenu();
+void getState();
+void choice(int drink);
+void check();
+void cancel();
+void cook();
+void finish();
 private:
-	int cash;
-	static const int menu_size = 5;
-	int nomer;
-	std::string menu[menu_size];
-	float prices[menu_size];
-	STATES state;
+int cash;
+static const int menu_size = 5;
+int nomer;
+std::string menu[menu_size];
+float prices[menu_size];
+STATES state;
 };
+#endif
